3405c09e83ce73be836def80edeefea3e76d26e4
oxonomy14 <oxonomy14@gmail.com>
2026-02-13 23:48:47 +0700

    added matches pattern for email

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -13,9 +13,14 @@ const RegistrationForm = () => {
         username: Yup.string()
             .min(2, 'Too Short!')
             .max(50, 'Too Long!').required('Required'),
-        email: Yup.string().email('Invalid email').required('Required'),
+      email: Yup.string()
+    .matches(
+      /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+      'Invalid email format'
+    )
+    .required('Required'),
         password: Yup.string()
-            .min(8, 'Password must be at least 8 characters')
+            .min(7, 'Password must be at least 7 characters')
             .required('Required'),
         confirmPassword: Yup.string()
             .oneOf([Yup.ref('password'), null], 'Passwords must match')
