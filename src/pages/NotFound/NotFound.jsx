5984f199ddc45557ddad0adaed60b8c9192da90a
oxonomy14 <oxonomy14@gmail.com>
2026-02-27 00:34:14 +0700

    fix bag in page NotFound

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -8,10 +8,18 @@ const NotFound = () => {
     <Container>
       <section className={css.sectionNotFound}>
         <div className={css.wrapper}>
-          <div className={css.error}>4 <div className={css.imgWrapper}><img src="img/404.png" alt="404 error" /></div> 4</div>
-          <p className={css.text}>Ooops! This page not found :(</p>
-          <Link className={css.link} to="/">To home page</Link>
+          <div className={css.error}>
+            4
+            <div className={css.imgWrapper}>
+              <img src="img/404.png" alt="404 error" />
+            </div>
+            4
           </div>
+          <p className={css.text}>Ooops! This page not found :(</p>
+          <Link className={css.link} to="/">
+            To home page
+          </Link>
+        </div>
       </section>
     </Container>
   );
