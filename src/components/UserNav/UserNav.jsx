b28188facc12fb46f59c29ee3823ea01a142f1d4
oxonomy14 <oxonomy14@gmail.com>
2026-03-04 21:51:54 +0700

    added responsive layout in UserNav

--- src/components/UserNav/UserNav.jsx
+++ src/components/UserNav/UserNav.jsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 import { selectUser } from '../../redux/auth/authSelector';
 import LogOutBtn from '../LogOutBtn/LogOutBtn';
 
-const UserNav = ({onLogoutClick, isTablet, isMobile}) => {
+const UserNav = ({onLogoutClick, isTablet, isMobile, isDesktop}) => {
   const location = useLocation();
   const isHomePage = location.pathname === '/';
 
@@ -13,11 +13,15 @@ const user = useSelector(selectUser);
 
 
   return (
-    <>
-      <div className={css.userNavItem}>
    
-        <LogOutBtn onLogoutClick={onLogoutClick} variant = 'userNav'/>
-        <div className={css.user}>
+      <div className={css.userNav}>
+   
+     {isDesktop && 
+    <div className={css.userNavItem}>
+      <LogOutBtn onLogoutClick={onLogoutClick} variant = 'userNav'/> 
+
+    
+    <div className={css.user}>
   {user.avatar ? (<img className={css.userImageAvatar} src={user.avatar} alt={user.name} />) : 
         (<svg
           width={24}
@@ -27,8 +31,24 @@ const user = useSelector(selectUser);
           <use href={`/icons/sprite.svg?v=${Date.now()}#icon-user`}></use>
         </svg>)}
         </div>
-       {!isMobile && <Link className={isHomePage ? css.userNameHome : css.userName} to="/profile">{user.name || 'User'}</Link>}
-       <div className={css.burgerWrapper}>
+      <Link className={isHomePage ? css.userNameHome : css.userName} to="/profile">{user.name || 'User'}</Link>
+     
+       </div> }
+
+{isTablet && 
+<div className={css.userNavItem}>
+<div className={css.user}>
+  {user.avatar ? (<img className={css.userImageAvatar} src={user.avatar} alt={user.name} />) : 
+        (<svg
+          width={24}
+          height={24}
+          className={css.userIcon}
+        >
+          <use href={`/icons/sprite.svg?v=${Date.now()}#icon-user`}></use>
+        </svg>)}
+        </div>
+      <Link className={isHomePage ? css.userNameHome : css.userName} to="/profile">{user.name || 'User'}</Link>
+        <div className={css.burgerWrapper}>
         <svg
           width={32}
           height={32}
@@ -38,8 +58,28 @@ const user = useSelector(selectUser);
           <use href={`/icons/sprite.svg#icon-burger`}></use>
         </svg>
        </div>
+</div>
+}
+
+
+
+    {isMobile && 
+    
+      <div className={css.burgerWrapper}>
+        <svg
+          width={32}
+          height={32}
+         
+          className={isHomePage ? css.burgerIconHome : css.burgerIcon}
+        >
+          <use href={`/icons/sprite.svg#icon-burger`}></use>
+        </svg>
+       </div>
+
+    }
+
       </div>
-    </>
+ 
   );
 };
 
