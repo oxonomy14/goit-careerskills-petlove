e3f6b942723629f8dcdf66ebe01d9e4c8657952d
oxonomy14 <oxonomy14@gmail.com>
2026-03-06 00:39:55 +0700

    added responsive layout in Pagination

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,44 +1,76 @@
 import css from "./Pagination.module.css";
+import { useMediaQuery } from 'react-responsive';
 
 const Pagination = ({ page, totalPages, onChange }) => {
-  const pages = [];
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1279 });
+  
 
-  // базові сторінки
-  for (let i = 1; i <= totalPages; i++) {
-    if (
-      i === 1 ||
-      i === totalPages ||
-      Math.abs(i - page) <= 1
-    ) {
-      pages.push(i);
+
+let pagination = [];
+
+  if (isMobile) {
+    pagination = [1];
+
+    if (totalPages >= 2) pagination.push(2);
+    if (totalPages > 2) pagination.push("...");
+
+  } else if (isTablet) {
+    pagination = [1];
+
+    if (totalPages >= 2) pagination.push(2);
+    if (totalPages >= 3) pagination.push(3);
+    if (totalPages > 3) pagination.push("...");
+
+  } else {
+    const pages = [];
+
+    for (let i = 1; i <= totalPages; i++) {
+      if (
+        i === 1 ||
+        i === totalPages ||
+        Math.abs(i - page) <= 1
+      ) {
+        pages.push(i);
+      }
+    }
+
+    let prev;
+
+    for (let p of pages) {
+      if (prev && p - prev > 1) {
+        pagination.push("...");
+      }
+      pagination.push(p);
+      prev = p;
     }
   }
 
-  // додаємо "..."
-  const pagination = [];
-  let prev;
-
-  for (let p of pages) {
-    if (prev && p - prev > 1) {
-      pagination.push('...');
-    }
-    pagination.push(p);
-    prev = p;
-  }
 
   return (
     <div className={css.pagination}>
       <button className={css.btnPagination} onClick={() => onChange(1)} disabled={page === 1}>
-        «
+          <svg className={css.leftIcon}>
+          <use href={`/icons/sprite.svg?v=${Date.now()}#icon-left`} />
+      </svg>
+            <svg className={css.leftIcon}>
+          <use href={`/icons/sprite.svg?v=${Date.now()}#icon-left`} />
+      </svg>
       </button>
 
       <button className={css.btnPagination} onClick={() => onChange(page - 1)} disabled={page === 1}>
-        ‹
+       <svg className={css.leftIcon}>
+          <use href={`/icons/sprite.svg?v=${Date.now()}#icon-left`} />
+      </svg>
+      
+
       </button>
+
+<div className={css.btnPaginationM}>
 
       {pagination.map((p, index) =>
   p === '...' ? (
-    <span key={`dots-${index}`}>…</span>
+    <span key={`dots-${index}`} className={css.btnDots}>…</span>
   ) : (
     <button
       key={`page-${p}-${index}`}
@@ -50,22 +82,29 @@ const Pagination = ({ page, totalPages, onChange }) => {
   )
 )}
 
-      
+  </div>    
 
       <button
         onClick={() => onChange(page + 1)}
         disabled={page === totalPages}
-        className={css.btnPagination}
+        className={css.btnPaginationR}
       >
-        ›
+         <svg className={css.rightIcon}>
+          <use href={`/icons/sprite.svg?v=${Date.now()}#icon-right`} />
+      </svg>
       </button>
 
       <button
         onClick={() => onChange(totalPages)}
         disabled={page === totalPages}
-        className={css.btnPagination}
+        className={css.btnPaginationR}
       >
-        »
+            <svg className={css.rightIcon}>
+          <use href={`/icons/sprite.svg?v=${Date.now()}#icon-right`} />
+      </svg>
+            <svg className={css.rightIcon}>
+          <use href={`/icons/sprite.svg?v=${Date.now()}#icon-right`} />
+      </svg>
       </button>
     </div>
   );
